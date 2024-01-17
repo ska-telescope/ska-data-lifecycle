78be5997e2dac35a6bedc08f66b40434d6e9a59e
Andreas Wicenec <awicenec@gmail.com>
2024-01-17 16:22:12 +0800

    Added complete structure

---
+++ src/ska_dlm/__init__.py
@@ -0,0 +1,13 @@
+"""DLM package for ska-data-lifecycle."""
+
+__all__ = [
+    DLMdb,
+    DLMingest,
+    DLMmigration,
+    DLMrequest,
+    DLMstorage,
+]
+
+__author__ = """Andreas Wicenec"""
+__email__ = "andreas.wicenec@icrar.org"
+__version__ = "0.0.1"
