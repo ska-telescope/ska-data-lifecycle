7083098c86fe7e764812009a89dd70285c649716
Andreas Wicenec <awicenec@gmail.com>
2024-01-18 21:15:47 +0800

    Init and better query now possible

--- src/ska_dlm/dlm_ingest/dlm_ingest_requests.py
+++ src/ska_dlm/dlm_ingest/dlm_ingest_requests.py
@@ -1,19 +1,36 @@
 """
 Convenience functions wrapping the most important postgREST API calls. 
 """
+import benedict
+import json
 import requests
 from .. import CONFIG
 
 
-def init_data_item(item_name: str, **kwargs) -> str:
+def init_data_item(item_name: str = "", json_data: str = "") -> str:
     """
     Intialize a new data_item by at least specifying an item_name.
 
     Parameters:
-    item_name
+    item_name, the item_name, can be empty, but then json_data has to be
+               specified.
+    json_data, provides to ability to specify all values.
+
+    Returns:
+    uid,
     """
-    api_url = f"{CONFIG.REST.base_url}/{CONFIG.DLM.dlm_table}"
-    return api_url
+    request_url = f"{CONFIG.REST.base_url}/{CONFIG.DLM.dlm_table}"
+    if item_name:
+        post_data = {"item_name": item_name}
+    elif json_data:
+        post_data = json_data
+    else:
+        print("Either item_name or json_data has to be specified!")
+        return None
+    r = requests.post(
+        request_url, json=post_data, headers={"Prefer": "missing=default, return=representation"}
+    )
+    return r.json()[0]["uid"]
 
 
 def set_uri(uid: str, uri: str):
--- src/ska_dlm/dlm_request/dlm_request_requests.py
+++ src/ska_dlm/dlm_request/dlm_request_requests.py
@@ -6,18 +6,22 @@ import requests
 from .. import CONFIG
 
 
-def query_data_item(item_name: str = "", **kwargs) -> str:
+def query_data_item(item_name: str = "", query_string: str = "") -> str:
     """
     Query a new data_item by at least specifying an item_name.
 
     Parameters:
-    item_name, could be empty, in which case the whole table is returned
+    item_name, could be empty, in which case the first 1000 items
+               are returned
+    query_string, an aribtrary postgREST query string
     """
-    api_url = f"{CONFIG.REST.base_url}/{CONFIG.DLM.dlm_table}"
+    api_url = f"{CONFIG.REST.base_url}/{CONFIG.DLM.dlm_table}?limit=1000"
     if item_name:
-        request_url = f"{api_url}?item_name=eq.{item_name}"
+        request_url = f"{api_url}&item_name=eq.{item_name}"
+    elif query_string:
+        request_url = f"{api_url}&{query_string}"
     else:
-        request_url = api_url
+        request_url = f"{api_url}"
     r = requests.get(request_url)
     if r.status_code == 200:
         return r.json()
