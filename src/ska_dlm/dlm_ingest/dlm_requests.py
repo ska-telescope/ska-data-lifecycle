e2c267e2f75704bca5615fff37e44529964bc7c5
Andreas Wicenec <awicenec@gmail.com>
2024-01-18 13:12:06 +0800

    added postgREST config

---
+++ src/ska_dlm/dlm_ingest/dlm_requests.py
@@ -0,0 +1,41 @@
+import requests
+
+
+def create_data_item(oid="", uid="", **kwargs) -> uid:
+    """ """
+    pass
+
+
+def set_uri(oid="", uid="", **kwargs) -> uid:
+    """ """
+    pass
+
+
+def set_state(oid="", uid="", **kwargs) -> uid:
+    """ """
+    pass
+
+
+def set_oid_expiration(oid="", **kwargs) -> None:
+    """ """
+    pass
+
+
+def set_uid_expiration(uid="", **kwargs) -> None:
+    """ """
+    pass
+
+
+def set_user(oid="", uid="", **kwargs) -> uid:
+    """ """
+    pass
+
+
+def set_group(oid="", uid="", **kwargs) -> uid:
+    """ """
+    pass
+
+
+def set_acl(oid="", uid="", **kwargs) -> uid:
+    """ """
+    pass
