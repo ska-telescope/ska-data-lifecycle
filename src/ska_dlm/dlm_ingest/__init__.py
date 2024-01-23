e27dcc0f0e886f2e2954d2032278b6dc1126f934
james-strauss-uwa <james.strauss@uwa.edu.au>
2024-01-23 12:04:49 +0800

    YAN-1591: Minor change to package import order

--- src/ska_dlm/dlm_ingest/__init__.py
+++ src/ska_dlm/dlm_ingest/__init__.py
@@ -1,13 +1,13 @@
 """DLMingest module for ska-data-lifecycle."""
 from .dlm_ingest_requests import (
     init_data_item,
-    set_uri,
-    set_state,
-    set_oid_expiration,
-    set_uid_expiration,
-    set_user,
-    set_group,
     set_acl,
+    set_group,
+    set_oid_expiration,
+    set_state,
+    set_uid_expiration,
+    set_uri,
+    set_user,
 )
 
 __all__ = [
