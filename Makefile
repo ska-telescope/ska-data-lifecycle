ad0d4b05e82d952010094c515ebd0d13894e7012
Nadia Steyn <nadia.steyn.bsc@gmail.com>
2024-01-30 14:53:58 +0800

    YAN-1643: Setup CI/CD environment
    - Start up test services in the local environment before starting tests
    - Make sure the test services are ready before running tests
    - changed postgres port from 5433 to 5432, as GitLab won't let us change the port, and 5432 is the default for the postgres image.

--- Makefile
+++ Makefile
@@ -11,3 +11,28 @@ docs-pre-build:
 
 .PHONY: docs-pre-build
 
+DOCKER_COMPOSE := docker compose
+POSTGREST_PID_FILE := .postgrest.pid
+
+# We use GitlabCI services in CI so only use docker compose locally
+python-pre-test:
+	[[ -z $$GITLAB_CI ]] \
+		&& $(MAKE) docker-compose-up \
+		|| echo "Not starting docker-compose containers in CI"
+
+	scripts/setup_services.sh $(POSTGREST_PID_FILE)
+
+python-post-test:
+	kill $$(cat $(POSTGREST_PID_FILE))
+
+	rm $(POSTGREST_PID_FILE)
+
+	[[ -z $$GITLAB_CI ]] \
+		&& $(MAKE) docker-compose-down \
+		|| echo "Not stopping docker-compose containers in CI"
+
+docker-compose-up:
+	$(DOCKER_COMPOSE) --file docker/test-services.docker-compose.yml up --detach
+
+docker-compose-down:
+	$(DOCKER_COMPOSE) --file docker/test-services.docker-compose.yml down
---
+++ scripts/setup_services.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+POSTGREST_PID_FILE=$1
+
+if ! command -v psql &> /dev/null; then
+    apt-get update -qq
+    apt-get install -y -qq postgresql-client
+fi
+
+MAX_RETRIES=5
+DELAY_SECONDS=1
+attempt=1
+while [[ $attempt -le $MAX_RETRIES ]]; do
+    PGPASSWORD=mysecretpassword psql -U postgres -h localhost -p 5432 -f setup/DB/ska_dlm_meta.sql > /dev/null
+    if [[ $? -eq 0 ]]; then
+    # successful connection
+        break
+    fi
+    echo "Attempt $attempt failed for postgres. Retrying in $DELAY_SECONDS seconds..."
+    sleep $DELAY_SECONDS
+    ((attempt++))
+done
+
+if [[ $attempt -gt $MAX_RETRIES ]]; then
+    echo "Max retries reached for postgres. Unable to establish connection."
+    exit 1
+fi
+
+
+postgrest setup/postgREST/postgREST.conf &> /dev/null &
+echo "$!">$POSTGREST_PID_FILE
+
+attempt=1
+while [[ $attempt -le $MAX_RETRIES ]]; do
+    curl -s http://localhost:3001 > /dev/null
+    if [[ $? -eq 0 ]]; then
+    # successful connection
+        break
+    fi
+    echo "Attempt $attempt failed for postgREST. Retrying in $DELAY_SECONDS seconds..."
+    sleep $DELAY_SECONDS
+    ((attempt++))
+done
+
+if [[ $attempt -gt $MAX_RETRIES ]]; then
+    echo "Max retries reached for postgREST. Unable to establish connection."
+    exit 1
+fi
