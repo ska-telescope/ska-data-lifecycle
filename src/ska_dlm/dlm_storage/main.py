40f8b8a6d5c93be9c50cef53f9d6b1e9045b08d3
james-strauss-uwa <james.strauss@uwa.edu.au>
2024-02-06 15:30:29 +0800

    YAN-1641: Added check for new items

--- src/ska_dlm/dlm_storage/main.py
+++ src/ska_dlm/dlm_storage/main.py
@@ -3,14 +3,22 @@
 import logging
 import os
 import sys
+from datetime import datetime
 from time import sleep
 
-from ska_dlm import dlm_ingest, dlm_request, dlm_storage
+from ska_dlm import dlm_migration, dlm_request, dlm_storage
 
 SLEEP_DURATION = 2  # seconds
 STORAGE_WARNING_PERCENTAGE = 80.0
 
 logger = logging.getLogger(__name__)
+last_new_data_item_query_time = ""
+
+
+def update_last_new_item_query_time():
+    """Update our record of the last time the database was queried for new items."""
+    global last_new_data_item_query_time
+    last_new_data_item_query_time = datetime.now().isoformat()
 
 
 def expire_uids():
@@ -18,10 +26,34 @@ def expire_uids():
     expired_data_items = dlm_request.query_expired()
 
     for uid in expired_data_items:
-        dlm_ingest.delete_data_item(uid=uid)
+        success = dlm_storage.delete_data_item_payload(uid)
+
+        if not success:
+            logger.warning("Unable to delete data item payload: %s", uid)
 
     if len(expired_data_items) > 0:
         logger.info("Expired %s data items", len(expired_data_items))
+
+
+def check_for_new_data_items():
+    """Check for new data items (since the last query), if found, copy to a second location."""
+    query = (
+        "uid_creation=gt."
+        + last_new_data_item_query_time
+        + "&item_phase=eq.GAS&item_state=eq.READY&select=uid,item_name,uri"
+    )
+    new_data_items = dlm_request.query_data_item(query_string=query)
+    update_last_new_item_query_time()
+
+    for new_data_item in new_data_items:
+        logger.info("Found new data item %s", new_data_item["item_name"])
+
+        # TODO: check phase?
+
+        # TODO: find a new location
+
+        # make a copy
+        dlm_migration.copy_data_item(uid=new_data_item["uid"])
 
 
 def check_storage_capacity():
@@ -49,7 +81,8 @@ def main():
     """Begin a long-running process."""
     while True:
         expire_uids()
-        check_storage_capacity()
+        check_for_new_data_items()
+        # check_storage_capacity()
         # perform_phase_transitions()
 
         sleep(SLEEP_DURATION)
