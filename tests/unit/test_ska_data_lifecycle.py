476af46a8bff775567da8f8bae50826e124a8b52
Andreas Wicenec <awicenec@gmail.com>
2024-02-07 22:18:35 +0800

    YAN-1654: Added complete storage addition test.

--- tests/unit/test_ska_data_lifecycle.py
+++ tests/unit/test_ska_data_lifecycle.py
@@ -123,3 +123,21 @@ class TestDlm(TestCase):
         res = data_item.set_uri(uid, f"{fpath}", storage_id)
         res = data_item.set_state(uid, "DELETED")
         assert res
+
+    def test_storage_config(self):
+        """Add a new location, storage and configuration to the rclone server."""
+        location_id = dlm_storage.init_location("MyHost", "Server")
+        assert len(location_id) == 36
+        config = '{"name":"MyDisk2","type":"local", "parameters":{}}'
+        uuid = dlm_storage.init_storage(
+            storage_name="MyDisk2",
+            location_id=location_id,
+            storage_type="disk",
+            storage_interface="posix",
+            storage_capacity=100000000,
+        )
+        assert len(uuid) == 36
+        config_id = dlm_storage.create_storage_config(uuid, config=config)
+        assert len(config_id) == 36
+        # configure rclone
+        assert dlm_storage.rclone_config(config) is True
