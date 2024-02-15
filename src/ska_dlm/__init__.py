d295dbaa7dd9478ccd910188205c8bc5f0c472a9
Rodrigo Tobar <rtobar@icrar.org>
2024-02-15 16:19:22 +0800

    YAN-1660 Remove DB section from configuration file
    All the ska-dlm libraries talk exclusively to PostgREST at this point in
    time, so there is no need to configuration direct database access for
    the ska-dlm libraries (and in fact this preference was indeed never
    used).
    
    This commit removes the DB section from the configuration file used by
    the ska-dlm libraries, and any further references to a separate secrets
    file. When/if we update these libraries to talk to the DB directly we
    can reinstate these preferences as necessary.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/ska_dlm/__init__.py
+++ src/ska_dlm/__init__.py
@@ -1,6 +1,5 @@
 """DLM package for ska-data-lifecycle."""
 
-import logging
 import os
 
 import benedict
@@ -14,25 +13,11 @@ __version__ = "0.0.1"
 
 DLM_PATH = os.path.dirname(__file__)
 
-logger = logging.getLogger(__name__)
-
 
 def read_config(cfg_file: str = f"{DLM_PATH}/config.yaml") -> dict:
     """Read the config file and return the config dictionary."""
     with open(cfg_file, "r", encoding="utf-8") as file:
-        cfg = benedict.benedict(yaml.safe_load(file))
-
-    # TODO: Check validity and defaults.
-    # cfg.REST.cfg_file = cfg.REST.cfg_file.format(**locals())
-    if cfg.DB.password.startswith("!"):
-        try:
-            with open(f"{DLM_PATH}/../../.secrets.yaml", "r", encoding="utf-8") as file:
-                secrets = benedict.benedict(yaml.safe_load(file))
-            if cfg.DB.password[1:] in secrets:
-                cfg.DB.password = secrets[cfg.DB.password[1:]]
-        except FileNotFoundError:
-            logger.error("secrets file not found!")
-    return cfg
+        return benedict.benedict(yaml.safe_load(file))
 
 
 CONFIG = read_config()
