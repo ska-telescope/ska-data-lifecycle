287ebcc2e58c81e4f5706358a6935e2f4a059d02
Andreas Wicenec <awicenec@gmail.com>
2024-02-15 19:13:13 +0800

    YAN-1661: Adjusted Sphinx doc generation

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -62,6 +62,7 @@ release = "0.1.1"
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
