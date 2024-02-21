83ca31360ce78ff840add4454afb8205d3f2c52c
Andreas Wicenec <awicenec@gmail.com>
2024-02-21 14:51:17 +0800

    YAN-1662:Removed print statement.

--- src/ska_dlm/dlm_storage/dlm_storage_requests.py
+++ src/ska_dlm/dlm_storage/dlm_storage_requests.py
@@ -102,7 +102,6 @@ def init_storage(  # pylint: disable=R0913
                 post_data.update({k: provided_args[k]})
             else:
                 raise InvalidQueryParameters(f"Argument {k} is mandatory!")
-    print(post_data)
     return DB.insert(CONFIG.DLM.storage_table, json=post_data)[0]["storage_id"]
 
 
