39e59318c19a1fbf5d4da27d23b2dfb89bd50b81
Nadia Steyn <nadia.steyn.bsc@gmail.com>
2024-07-01 14:30:19 +0800

    YAN-1761: isort take#2

--- src/ska_dlm/dlm_request/__init__.py
+++ src/ska_dlm/dlm_request/__init__.py
@@ -1,8 +1,14 @@
 """DLMrequest module for ska-data-lifecycle."""
 
-from .dlm_request_requests import (query_data_item, query_deleted, query_exists,
-                                   query_exists_and_ready, query_expired, query_item_storage,
-                                   query_new)
+from .dlm_request_requests import (
+    query_data_item,
+    query_deleted,
+    query_exists,
+    query_exists_and_ready,
+    query_expired,
+    query_item_storage,
+    query_new,
+)
 
 __all__ = [
     "query_data_item",
--- src/ska_dlm/dlm_storage/__init__.py
+++ src/ska_dlm/dlm_storage/__init__.py
@@ -1,9 +1,19 @@
 """DLMstorage module for ska-data-lifecycle."""
 
-from .dlm_storage_requests import (check_item_on_storage, check_storage_access,
-                                   create_storage_config, delete_data_item_payload, delete_uids,
-                                   get_storage_config, init_location, init_storage, query_location,
-                                   query_storage, rclone_access, rclone_config)
+from .dlm_storage_requests import (
+    check_item_on_storage,
+    check_storage_access,
+    create_storage_config,
+    delete_data_item_payload,
+    delete_uids,
+    get_storage_config,
+    init_location,
+    init_storage,
+    query_location,
+    query_storage,
+    rclone_access,
+    rclone_config,
+)
 
 __all__ = [
     "check_item_on_storage",
