ae070de9e60b838d6fb22638fcba7cc7cdfe630e
Callan Gray <callan.gray@icrar.org>
2024-08-09 18:21:23 +0800

    YAN-1799 helm config loading

--- src/ska_dlm/__init__.py
+++ src/ska_dlm/__init__.py
@@ -1,8 +1,8 @@
 """DLM package for ska-data-lifecycle."""
 
-import os
-
-import benedict
+from pathlib import Path
+import shutil
+from benedict import benedict
 import yaml
 
 # from . import dlm_db, dlm_ingest, dlm_migration, dlm_request, dlm_storage
@@ -12,19 +12,30 @@ __author__ = """Andreas Wicenec"""
 __email__ = "andreas.wicenec@icrar.org"
 __version__ = "0.0.1"
 
-DLM_PATH = os.path.dirname(__file__)
+DLM_LIB_DIR = Path(__file__).parent
+"""The library install path of dlm."""
+
+DLM_HOME = Path.home() / ".dlm/"
+"""The configuration path of dlm."""
 
 
-def read_config(cfg_file: str = f"{DLM_PATH}/config.yaml") -> dict:
+def read_config(user_config_file: Path = DLM_HOME / "config.yaml") -> benedict:
     """Read the config file and return the config dictionary."""
-    with open(cfg_file, "r", encoding="utf-8") as file:
-        return benedict.benedict(yaml.safe_load(file))
+    if not user_config_file.exists():
+        # create the default user config in DLM_HOME if it does not already exist
+        user_config_file.parent.mkdir(exist_ok=True)
+        default_user_config_file = DLM_LIB_DIR / "config.yaml"
+        shutil.copy(default_user_config_file, user_config_file)
+
+    with open(user_config_file, "r", encoding="utf-8") as file:
+        return benedict(yaml.safe_load(file))
 
 
 CONFIG = read_config()
 
 __all__ = [
-    "DLM_PATH",
+    "DLM_HOME",
+    "DLM_LIB_DIR",
     "CONFIG",
     "dlm_db",
 ]
