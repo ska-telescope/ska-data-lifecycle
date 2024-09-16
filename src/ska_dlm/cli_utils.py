811eea4f452ed1c2ba6e53c89e08358af7791ee3
Callan Gray <callan.gray@icrar.org>
2024-09-16 11:32:31 +0800

    YAN-1799 simplify docgen function

--- src/ska_dlm/cli_utils.py
+++ src/ska_dlm/cli_utils.py
@@ -3,7 +3,6 @@
 import copy
 import functools
 import inspect
-import re
 import typing
 
 import typer
@@ -63,11 +62,22 @@ def add_as_typer_command(
     app.command()(typer_docstring(func))
 
 
-PARAGRAPH_RE = re.compile(r"(.)\n(?!\n)")
+def create_param(
+    class_type: type,
+    **param_kwargs,
+) -> typer.models.ParameterInfo:
+    """Create a typer parameter annotation."""
+    match class_type:
+        case typer.models.ArgumentInfo:
+            return typer.Argument(**param_kwargs, show_default=False)
+        case typer.models.OptionInfo:
+            return typer.Option(**param_kwargs)
+        case _:
+            raise NotImplementedError()
 
 
-def typer_docstring(func: typing.Callable) -> typing.Callable:
-    """A decorator that generates typer docs from the function signature and docstring.
+def typer_docstring(func: typing.Callable) -> typing.Callable:  # noqa: C901
+    """Decorates a function with typer docs from the function signature and docstring.
 
     Parameters
     ----------
@@ -80,14 +90,14 @@ def typer_docstring(func: typing.Callable) -> typing.Callable:
         Decorated function with updated annotations and docstring.
     """
     # Collect information about the parameters of the function
-    parameters: dict[str, dict] = {}
-    kinds: dict[str, type] = {}
+    kwargs: dict[str, dict] = {}
+    annotation_types: dict[str, type] = {}
 
     # Parse signature first so all parameter names are populated
     signature = inspect.signature(func)
     for arg_name, param in signature.parameters.items():
-        parameters[arg_name] = {}
-        kinds[arg_name] = (
+        kwargs[arg_name] = {}
+        annotation_types[arg_name] = (
             typer.models.ArgumentInfo
             if param.default is inspect.Signature.empty
             else typer.models.OptionInfo
@@ -97,29 +107,19 @@ def typer_docstring(func: typing.Callable) -> typing.Callable:
     assert func.__doc__
     docstring: Docstring = parse(func.__doc__)
 
-    # extract as help messages from docstring (if present)
+    # extract descriptions as help messages from docstring (if present)
     for par in docstring.params:
-        if par.arg_name in parameters:
-            parameters[par.arg_name]["help"] = par.description
+        if par.arg_name in kwargs:
+            kwargs[par.arg_name]["help"] = par.description
 
-    def create_param(
-        kind: type,
-        **param_kwargs,
-    ) -> typer.models.ParameterInfo:
-        match kind:
-            case typer.models.ArgumentInfo:
-                return typer.Argument(**param_kwargs, show_default=False)
-            case _:
-                return typer.Option(**param_kwargs)
-
-    # Transform the parameters into info instances
-    docstring_infos: dict[str, typer.models.ParameterInfo] = {
-        arg_name: create_param(kinds[arg_name], **param_kwargs)
-        for arg_name, param_kwargs in parameters.items()
+    # Create annotations
+    annotations: dict[str, typer.models.ParameterInfo] = {
+        arg_name: create_param(annotation_types[arg_name], **param_kwargs)
+        for arg_name, param_kwargs in kwargs.items()
     }
 
     output_annotations: dict = {}
-    for arg_name, info in docstring_infos.items():
+    for arg_name, annotation in annotations.items():
         updated_annotation: type | typing.Annotated = copy.deepcopy(func.__annotations__[arg_name])
         if hasattr(updated_annotation, "__metadata__"):
             updated = False
@@ -127,30 +127,20 @@ def typer_docstring(func: typing.Callable) -> typing.Callable:
                 if isinstance(meta, typer.models.ParameterInfo):
                     # override missing help with docstring
                     if meta.help is None:
-                        meta.help = copy.deepcopy(info.help)
+                        meta.help = copy.deepcopy(annotation.help)
 
                     updated = True
             if not updated:
                 # annotations found but not for typer
-                updated_annotation.__metadata__ += (info,)
+                updated_annotation.__metadata__ += (annotation,)
         else:
             # create annotation
-            # NOTE:
-            updated_annotation = typing.Annotated[updated_annotation, info]
+            updated_annotation = typing.Annotated[updated_annotation, annotation]
         output_annotations[arg_name] = updated_annotation
 
     if "return" in output_annotations:
         output_annotations["return"] = func.__annotations__["return"]
 
     func.__annotations__ = output_annotations
-
-    # Only keep the main description as docstring so the CLI won't print
-    # the whole docstring, including the parameters
-    func.__doc__ = "\n\n".join(
-        [
-            docstring.short_description or "",
-            PARAGRAPH_RE.sub(r"\1 ", docstring.long_description or ""),
-        ]
-    )
-
+    func.__doc__ = docstring.description
     return func
--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -1,14 +1,10 @@
 """Cli test module."""
 
-from typing import Annotated, Any
 
 import pytest
-import typer
-from typer.models import ArgumentInfo, OptionInfo
 from typer.testing import CliRunner
 
 from ska_dlm.cli import app
-from ska_dlm.cli_utils import typer_docstring
 
 runner = CliRunner()
 
@@ -17,66 +13,3 @@ runner = CliRunner()
 def test_cli_help():
     """Test CLI help loads."""
     runner.invoke(app, ["--help"])
-
-
-def mock_command(  # pylint: disable=unused-argument, too-many-arguments
-    arg1: str,
-    arg2: Annotated[str, typer.Argument(help="custom arg2 description")],
-    arg3: Annotated[str, "some other annotation"],
-    arg4: Annotated[str, typer.Argument()] = "default",
-    opt1: str = "",
-    opt2: str | None = None,
-    opt3: Annotated[str, typer.Option(help="custom opt3 description")] = "abcd",
-    opt4: Annotated[Any | None, typer.Option()] = None,
-):
-    """
-    Short description.
-
-    Long description across
-    multiple lines.
-
-    Parameters
-    ----------
-    arg1 : str
-        arg1 description
-    arg2 : str
-        arg2 description
-    arg3 : str
-        arg3 description
-    arg4 : str
-        arg4 description
-    opt1 : dict | list
-        opt1 description
-    opt2 : dict | list
-        opt2 description
-    opt3 : dict | list
-        opt3 description
-    opt4 : dict | list
-        opt4 description
-    """
-
-
-def test_typerdoc_generator():
-    """Test typer doc generator."""
-    # pylint: disable=no-member
-    typed = typer_docstring(mock_command)
-    assert typed.__doc__ == ("Short description.\n\nLong description across multiple lines.")
-    ann = typed.__annotations__
-    assert_parameter_info(ann["arg1"].__metadata__[0], ArgumentInfo, "arg1 description")
-    assert_parameter_info(ann["arg2"].__metadata__[0], ArgumentInfo, "custom arg2 description")
-    assert_parameter_info(ann["arg3"].__metadata__[1], ArgumentInfo, "arg3 description")
-    assert_parameter_info(ann["arg4"].__metadata__[0], ArgumentInfo, "arg4 description")
-
-    assert_parameter_info(ann["opt1"].__metadata__[0], OptionInfo, "opt1 description")
-    assert_parameter_info(ann["opt2"].__metadata__[0], OptionInfo, "opt2 description")
-    assert_parameter_info(ann["opt3"].__metadata__[0], OptionInfo, "custom opt3 description")
-    assert_parameter_info(ann["opt4"].__metadata__[0], OptionInfo, "opt4 description")
-
-
-def assert_parameter_info(
-    parameter_info: typer.models.ParameterInfo, info_type: type, helpdoc: str
-):
-    """Assert ParameterInfo members match arguments."""
-    assert isinstance(parameter_info, info_type)
-    assert parameter_info.help == helpdoc
-    assert parameter_info.default == ...
