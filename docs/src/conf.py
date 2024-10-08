ee67b4f003108eca7d4cfd49974d0fdfb68fe8d2
Callan Gray <callan.gray@icrar.org>
2024-10-08 12:52:54 +0800

    REL-1761 minor version fixes

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -51,9 +51,9 @@ author = "SKA Observatory"
 # built documents.
 #
 # The short X.Y.Z version.
-version = "0.1.1"
+version = "0.1.0"
 # The full version, including alpha/beta/rc pre-release tags.
-release = "0.1.1"
+release = "0.1.0"
 
 # -- General configuration ------------------------------------------------
 
