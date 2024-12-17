df0ec0566c2d2bd62164ba9c25f093395afe441b
james-strauss-uwa <james.strauss@uwa.edu.au>
2024-12-17 13:32:30 +0800

    YAN-1942: Fixed linting

--- src/ska_dlm/dlm_migration/__init__.py
+++ src/ska_dlm/dlm_migration/__init__.py
@@ -1,4 +1,4 @@
 """DLMmigration module for ska-data-lifecycle."""
-from .dlm_migration_requests import copy_data_item, rclone_copy, query_migrations
+from .dlm_migration_requests import copy_data_item, query_migrations, rclone_copy
 
 __all__ = ["copy_data_item", "rclone_copy", "query_migrations"]
