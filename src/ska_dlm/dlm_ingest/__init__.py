e8e8297ae4e92f6938900e163577618ce35d202f
Nadia Steyn <nadia.steyn.bsc@gmail.com>
2025-02-19 13:46:06 +0800

    YAN-1998: Build fix (?)

--- src/ska_dlm/dlm_ingest/__init__.py
+++ src/ska_dlm/dlm_ingest/__init__.py
@@ -1,5 +1,5 @@
 """DLM ingest module for ska-data-lifecycle."""
 
-from .dlm_ingest_requests import init_data_item, notify_data_dashboard, register_data_item
+from .dlm_ingest_requests import init_data_item, register_data_item
 
-__all__ = ["init_data_item", "register_data_item", "notify_data_dashboard"]
+__all__ = ["init_data_item", "register_data_item"]
