18439bb9da7536c32e5f3930577d8a3b46aeaf3d
dpallot <dave.pallot@uwa.edu.au>
2025-11-24 13:14:32 +0800

    DMAN-177: Ensure DLM schema is primary schema

--- setup/DB/patches/v1.1.2/alter_data_item_metadata_to_jsonb.sql
+++ setup/DB/patches/v1.1.2/alter_data_item_metadata_to_jsonb.sql
@@ -1,7 +1,9 @@
 -- Convert the 'metadata' column in 'data_item' from JSON to JSONB
 -- Safe to re-run: if already JSONB, this is a no-op
 -- Assumes the column is currently either JSON or JSONB
-SELECT current_setting('search_path') AS search_path;
+
+-- As this is the first script to run in the patch, its must set the schema its working on
+SET search_path TO dlm;
 
 ALTER TABLE data_item
 ALTER COLUMN metadata
